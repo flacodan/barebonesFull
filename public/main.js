ca120e1145cb596ab5058e55becac91fe7503444
Dan Hill <danshill@gmail.com>
2023-12-15 12:45:18 -0700

    added axios and temp db

--- public/main.js
+++ public/main.js
@@ -1,1 +1,33 @@
-console.log('hello');
+
+const createForm = document.getElementById('createForm');
+const nameInput = document.getElementById('create-name-input');
+const powerInput = document.getElementById('create-power-input');
+const healthInput = document.getElementById('create-health-input');
+
+createForm.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+
+    if (+powerInput.value < 0) {
+        alert('Wrong power')
+        return;
+    }
+
+    if (+healthInput.value < 0) {
+        alert('needs health')
+        return;
+    }
+
+    let maBod = {
+        name: nameInput.value,
+        power: powerInput.value,
+        health: healthInput.value
+    }
+
+    axios.post('/create-fighter', maBod)
+    .then((response) => {
+        console.log(response.data);
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+})
--- server.js
+++ server.js
@@ -3,6 +3,9 @@ import { dirname, join } from 'path';
 import { fileURLToPath } from 'url';
 
 let app = express();
+app.use(express.json());
+
+let db = []; //our fake db
 
 
 app.get('/', (req, res) => {
@@ -16,6 +19,12 @@ app.get('/css', (req, res) => {
 app.get('/js', (req, res) => {
     res.sendFile(join(dirname(fileURLToPath(import.meta.url)), '/public/main.js'))
 })
+
+app.post('/create-fighter', (req, res) => {
+    db.push(req.body);
+    res.status(200).send(db) //status 200 is default so we didn't need to put the status call for success, it's just an example
+})
+
 app.listen(8080, () => {
     console.log('Server listening on port 8080');
 })
